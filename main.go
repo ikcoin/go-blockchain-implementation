b5259d055d8f7a55346c3998550f55d4d826dd81
ikcoin <ikcoin.kb@protonmail.com>
2021-09-20 18:08:05 +0200

    fmt format

--- main.go
+++ main.go
@@ -10,20 +10,22 @@ import (
 func main() {
 
 	chain := blockchain.InitBlockchain()
-	fmt.Println(string(chain.Blocks[0].Data))
 
 	chain.AddBlock("First Block")
 	chain.AddBlock("Second Block")
 	chain.AddBlock("Third Block")
+	fmt.Println()
 
-	for _, block := range chain.Blocks {
+	fmt.Println("-----BLOCKCHAIN:------")
+	for blockNum, block := range chain.Blocks {
+		fmt.Println("BLOCK ", blockNum)
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data: %x\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println("-----------")
 
 		pow := blockchain.NewProof(block)
 		fmt.Println("Pow: ", strconv.FormatBool(pow.Validate()))
+		fmt.Println("-----------")
 		fmt.Println()
 	}
 
