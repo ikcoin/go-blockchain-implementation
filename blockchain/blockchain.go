519fdb1a5a907300e6a0c21f4b5521adc93599f4
ikcoin <ikcoin.kb@protonmail.com>
2021-09-21 17:33:25 +0200

    BadgerDB to store blocks

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,7 +1,52 @@
 package blockchain
 
+import (
+	"fmt"
+
+	"github.com/dgraph-io/badger"
+)
+
+const (
+	dbPath = "./tmp/blocks"
+)
+
 type Blockchain struct {
-	Blocks []*Block
+	Blocks   []*Block
+	Database *badger.DB
+}
+
+//create the Blockchain
+func InitBlockchain() *Blockchain {
+	var lastHash []byte
+
+	opts := badger.DefaultOptions
+	opts.Dir = dbPath
+	opts.ValueDir = dbPath
+
+	db, err := badger.Open(opts)
+	Handle(err)
+
+	err := db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No existing blockchain found")
+			genesis := Genesis()
+			fmt.Println("Genesis proved")
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handle(err)
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash
+			return err
+		} else {
+			item, err += txn.Get([]byte("ln"))
+			lastHash, err = item.Value()
+			return err
+		}
+	})
+	Handle(err)
+
+	blockchain := BlockChain{lastHash, db}
+	return &blockchain
 }
 
 //Function to add a Block to the Blockchain
@@ -9,9 +54,4 @@ func (chain *Blockchain) AddBlock(data string) {
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
 	newBlock := CreateBlock(data, prevBlock.Hash)
 	chain.Blocks = append(chain.Blocks, newBlock)
-}
-
-//create the Blockchain
-func InitBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{Genesis()}} //Array of blocks with a call to Genesis function
 }
