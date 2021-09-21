0638ae693d108ba3a316ded26cd8c2af88d307d8
ikcoin <ikcoin.kb@protonmail.com>
2021-09-21 17:08:24 +0200

    delete function

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -11,11 +11,6 @@ func (chain *Blockchain) AddBlock(data string) {
 	chain.Blocks = append(chain.Blocks, newBlock)
 }
 
-//Function to create the GenesisBlock(first Block) of the Blockchain
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
 //create the Blockchain
 func InitBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{Genesis()}} //Array of blocks with a call to Genesis function
