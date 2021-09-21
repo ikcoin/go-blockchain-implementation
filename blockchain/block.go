7c0c3c94b332089128815d096cb8d80fb22c39ae
ikcoin <ikcoin.kb@protonmail.com>
2021-09-21 17:12:36 +0200

    Serialize & Deserialize functions

--- blockchain/block.go
+++ blockchain/block.go
@@ -3,6 +3,8 @@ package blockchain
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"log"
 )
 
 type Block struct {
@@ -38,4 +40,30 @@ func CreateBlock(data string, prevHash []byte) *Block {
 //Function to create the GenesisBlock(first Block) of the Blockchain
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
+}
+
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	if err != nil {
+		log.Panic(err)
+	}
+	return &block
 }
